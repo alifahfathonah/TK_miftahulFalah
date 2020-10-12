eebadf9b04973044130ce63fcdaae5cd6c517b45
wendiNurhermansah <wendii.2mia3@gmail.com>
2020-10-12 22:18:34 +0700

    initial master

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,208 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.14.0/css/all.css" integrity="sha384-HzLeBuhoNPvSl5KYnjx0BT+WB0QEEqLprO+NBkkk5gbc67FTaL7XIGa2w1L0Xbgc" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="{{ asset('css/blog.css') }}">
+
+    <title>SELAMAT DATANG !</title>
+
+   
+
+  
+  </head>
+  <body>
+      
+
+
+    <nav class="navbar navbar-expand-lg navbar-light bg-info fixed-top">
+      <i class="fas fa-graduation-cap"></i>
+      <a class="navbar-brand font-weight-bold ml-2" href="#">MIFTAHUL-FALLAH</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="container font-weight-bold p-2">
+        <div class="collapse navbar-collapse ml-2" id="navbarSupportedContent">
+        
+          <ul class="navbar-nav mr-auto">
+            <li class="nav-item active">
+              <a class="nav-link mr-5" href="#">Home <span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item dropdown">
+              <a class="nav-link dropdown-toggle mr-2" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                Profile
+              </a>
+              <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                <a class="dropdown-item" href="#"><i class="fas fa-angle-right"></i>Sejarah</a>
+                <a class="dropdown-item" href="#"><i class="fas fa-angle-right"></i>Legalitas</a>
+                <a class="dropdown-item" href="#"><i class="fas fa-angle-right"></i>Visi dan Misi</a>
+                <a class="dropdown-item" href="#"><i class="fas fa-angle-right"></i>Alamat</a>
+              </div>
+            </li>
+            <li class="nav-item dropdown">
+              <a class="nav-link dropdown-toggle mr-2" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                Akademik
+              </a>
+              <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                <a class="dropdown-item" href="#"><i class="fas fa-angle-right"></i>Kurikulum</a>
+                <a class="dropdown-item" href="#"><i class="fas fa-angle-right"></i>Metode Pengajaran</a>
+                <a class="dropdown-item"><i class="fas fa-angle-right"></i>Tenaga pendidik</a>
+                <a class="dropdown-item" href="#"><i class="fas fa-angle-right"></i>Data Siswa Aktif</a>
+                <a class="dropdown-item" href="#"><i class="fas fa-angle-right"></i>Data Alumni</a>
+              </div>
+            </li>
+            <li class="nav-item dropdown">
+              <a class="nav-link dropdown-toggle mr-2" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                Fasilitas
+              </a>
+              <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                <a class="dropdown-item" href="#"><i class="fas fa-angle-right"></i>?</a>
+                
+              </div>
+            </li>
+            <li class="nav-item dropdown">
+              <a class="nav-link dropdown-toggle mr-2" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                Kegiatan
+              </a>
+              <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                <a class="dropdown-item" href="#"><i class="fas fa-angle-right"></i>R.A</a>
+                <a class="dropdown-item" href="#"><i class="fas fa-angle-right"></i>Paud</a>
+               
+              </div>
+            </li>
+            <li class="nav-item dropdown">
+              <a class="nav-link dropdown-toggle mr-2" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                Pendaftaran
+              </a>
+              <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                <a class="dropdown-item mr-2" href="#"><i class="fas fa-angle-right"></i>Informasi Pendaftaran</a>
+               
+              </div>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link disabled mr-2" href="#" tabindex="-1" aria-disabled="true">Prestasi</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link disabled mr-2" href="#" tabindex="-1" aria-disabled="true">Kontak</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link disabled mr-2" href="#" tabindex="-1" aria-disabled="true">Lihat Rute</a>
+            </li>
+            <div class="mt-2 ml-5">
+                  <h5>
+                    <i class="fab fa-instagram-square " data-toggle="tooltip" title="instagram"></i>
+                    <i class="fab fa-facebook ml-4" data-toggle="tooltip" title="facebook"></i>
+                  </h5>
+            </div>
+          </ul>
+         
+        </div>
+      </div>
+    </nav>
+
+  
+    
+    
+  <div class="col-md-12 center-auto ">
+      <div id="carouselExampleCaptions" class="carousel slide" data-ride="carousel">
+        <ol class="carousel-indicators">
+          <li data-target="#carouselExampleCaptions" data-slide-to="0" class="active"></li>
+          <li data-target="#carouselExampleCaptions" data-slide-to="1"></li>
+          <li data-target="#carouselExampleCaptions" data-slide-to="2"></li>
+        </ol>
+        <div class="carousel-inner">
+          <div class="carousel-item active">
+             <div class="carousel-caption d-none d-md-block">
+              <h2 class="font-weight-bold">SELAMAT DATANG DI RA \ PAUD </h2>
+              <h1>MIFTAHUL-FALAH</h1>
+            </div>
+            <img src="{{ asset('assets/img/slide/w3.jpg') }}" class="d-block w-100" alt="...">
+           
+          </div>
+          <div class="carousel-item">
+            <img src="{{ asset('assets/img/slide/w1.jpg') }}" class="d-block w-100" alt="...">
+            <div class="carousel-caption d-none d-md-block">
+              <h2>SELAMAT DATANG DI RA \ PAUD </h2>
+              <h1>MIFTAHUL-FALAH</h1>
+            </div>
+          </div>
+          <div class="carousel-item">
+            <img src="{{ asset('assets/img/slide/w2.jpg') }}" class="d-block w-100" alt="...">
+            <div class="carousel-caption d-none d-md-block mt-2">
+             <h2>SELAMAT DATANG DI RA \ PAUD </h2>
+              <h1>MIFTAHUL-FALAH</h1>
+            </div>
+          </div>
+        </div>
+        <a class="carousel-control-prev" href="#carouselExampleCaptions" role="button" data-slide="prev">
+          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+          <span class="sr-only">Previous</span>
+        </a>
+        <a class="carousel-control-next" href="#carouselExampleCaptions" role="button" data-slide="next">
+          <span class="carousel-control-next-icon" aria-hidden="true"></span>
+          <span class="sr-only">Next</span>
+        </a>
+      </div>
+  </div>
+
+  <footer class="bg-dark text-white p-5">
+        <div class="row">
+          <div class="col-md-3">
+            <h5>SEKOLAH KAMI :</h5>
+            <ul>
+              <li></li>
+              <li></li>
+              <li></li>
+              <li></li>
+            </ul>
+          </div>
+          <div class="col-md-3">
+            <h5>TENTANG SEKOLAH KAMI :</h5>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+            tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+            quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+            </p>
+          </div>
+          <div class="col-md-3">
+            <h5>NAMA YAYASAN :</h5>
+            <ul>
+              <li></li>
+              <li></li>
+              <li></li>
+            </ul>
+          </div>
+          <div class="col-md-3">
+            <h5>HUBUNGI KAMI :</h5>
+            <ul>
+              <li></li>
+              <li></li>
+              <li></li>
+
+            </ul>
+          </div>
+        </div>
+      </footer>
+     
+       <div class="copyright text-center text-white font-weight-bold bg-warning p-4"> 
+        <P>RA/PAUD MIFTAHUL-FALAH || copyright <i class="far fa-copyright">2020</i></P>
+      </div>
+  
+
+
+
+
+
+
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+  </body>
+</html>
